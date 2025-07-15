481646186552f824cbd1789143f663c975857440
Neloy-SWE <taufiqneloy.swe@gmail.com>
2025-07-15 22:05:49 +0600

    my app widget added

---
+++ lib/my_app.dart
@@ -0,0 +1,10 @@
+/*
+* Created by Neloy on 14 July, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+import 'package:flutter/material.dart';
+import 'package:practice_bloc_flutter_infinite_list/posts/posts.dart';
+class MyApp extends MaterialApp{
+  const MyApp({super.key}) : super(home: const PostsPage());
+}
